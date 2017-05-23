034de087b4097f7e154b2599a5cde9ba9e9d468c
rujaraju <jan.rundt@gmail.com>
2017-05-23 11:09:02 +0000

    beginning

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var path = require("path");
+var app = express();
+var request = require('request')
+var mongo = require('mongodb').MongoClient;
+
+
+app.get('/imagesearch', function(req, res) {
+  
+})
+
+
+//api-key AIzaSyDoWMgf_idhENTMBQt-cT0T0PapDXUzzKo
+
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+})
